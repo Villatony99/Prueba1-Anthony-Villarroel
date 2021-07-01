22d8ed78cb7f88107b929c15b240f581a76142d3
Anthony Villarroel <60784810+Villatony99@users.noreply.github.com>
2021-07-01 15:07:03 -0500

    Prueba 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express =require('express');
 
+
 //generar aplicacion express
 const app = express()
 
--- routers/index.js
+++ routers/index.js
@@ -1,21 +1,37 @@
 const { Router } = require('express')
-const axios = require("axios");
-const {FetchData} = require('../lib/data')
+const { FetchData } = require('../lib/data')
 
 const router = Router()
 
-
-
 router
-.get("/",(req,res)=>{
-    res.send("Prueba1: Anthony Villarroel")
-})
-.get("/endopoint1/:params",(req,res)=>{
-    const { params: {id} } = req
-
-(async()=>{
-    console.log(FetchData.getByPage(id))
-})()
-})
+    .get("/", (req, res) => {
+        res.send("Prueba1: Anthony Villarroel")
+    })
+    .get("/endpoint1/:id", async (req, res) => {
+        try {
+            const { params: { id } } = req
+            const param = await FetchData.getCharacter(id)
+            res.json({
+                tema: `Datos del persona ${id}`,
+                body: param
+            })
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    })
+    .get("/endpoint2", async (req, res) => {
+        try {
+            const { query } = req
+            //console.log(query.id)
+            const param = await FetchData.getSpecificCharacters(5,6)
+            console.log(param)
+            res.json({
+                tema: `Datos del persona ${id}`,
+                body: param
+            })
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    })
 
 module.exports.RouterIndex = router
